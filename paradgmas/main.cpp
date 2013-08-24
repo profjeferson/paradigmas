2f07bdf21287ae5dac3e4e31bfca0a5ebdef62a2
jeferson <jeferso.mab@gmail.com>
2013-08-24 00:33:14 -0300

    Exemplo de programação em C++. Olá mundo.

---
+++ paradgmas/main.cpp
@@ -0,0 +1,12 @@
+// Um programa elementar.
+#include <iostream>;
+using namespace std;
+
+int main() {
+    cout << "Alo, Mundo!\n";
+    string nome = "";
+    cout << "Digite seu nome:";
+    cin >> nome;
+    cout <<"Olá "<<nome;
+    return 0;
+}
---
+++ paradgmas/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=paradgmas
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/paradgmas
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=paradgmas.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/paradgmas.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=paradgmas
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/paradgmas
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=paradgmas.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/paradgmas.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
